f9c443f2f150be36684fdd97b67905e9b8c51f99
Ishan Shah <ishan0102@utexas.edu>
2023-05-08 19:40:08 -0500

    Add Google analytics

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Script from 'next/script'
 import { Countdown } from '../components/Countdown'
 
 export default function home() {
@@ -6,9 +7,29 @@ export default function home() {
     <>
       <Countdown/>
       <Head>
-        <title>Countdowns</title>
+        <title>countdowns</title>
         <link rel="icon" href="/static/favicon.png" />
+
       </Head>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script 
+        strategy="afterInteractive" 
+        src={"https://www.googletagmanager.com/gtag/js?id=G-KJ2KXYRCND"}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-KJ2KXYRCND', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
     </>
   )
 }
