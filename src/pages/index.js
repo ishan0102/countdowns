db837cb60db64777fd32ec1903827359db5464d7
Ishan Shah <ishan0102@utexas.edu>
2023-05-10 22:06:05 -0500

    Add Google AdSense

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,26 +10,34 @@ export default function home() {
         <title>countdowns</title>
         <link rel="icon" href="/static/favicon.png" />
 
+        {/* Google AdSense */}
+        <Script
+          id="google-adsense"
+          strategy="afterInteractive"
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3670547524946981"
+          crossOrigin="anonymous"
+        />
+
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <Script 
+          strategy="afterInteractive" 
+          src={"https://www.googletagmanager.com/gtag/js?id=G-KJ2KXYRCND"}
+        />
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-KJ2KXYRCND', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
       </Head>
-      {/* Global Site Tag (gtag.js) - Google Analytics */}
-      <Script 
-        strategy="afterInteractive" 
-        src={"https://www.googletagmanager.com/gtag/js?id=G-KJ2KXYRCND"}
-      />
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-KJ2KXYRCND', {
-              page_path: window.location.pathname,
-            });
-          `,
-        }}
-      />
     </>
   )
 }
