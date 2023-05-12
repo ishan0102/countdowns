a1cc792ef9dee1548c2e620889f7fc0630792f9d
Ishan Shah <ishan0102@utexas.edu>
2023-05-12 02:31:20 -0500

    Add basic SEO

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,26 @@
 import '../styles/globals.css'
+import { DefaultSeo } from 'next-seo'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <DefaultSeo
+        openGraph={{
+          type: 'website',
+          url: 'https://www.countdowns.live',
+          title: 'countdowns',
+          description: 'Make countdowns and share them with your friends.',
+          images: [
+            {
+              url: '/static/img/thumbnail.png',
+              width: 3456,
+              height: 1922,
+              alt: 'countdowns.live homepage',
+            },
+          ],
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
