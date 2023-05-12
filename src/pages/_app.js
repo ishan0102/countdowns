1e89d91d926fef81c3d6059c6f1576e7afffd669
Ishan Shah <ishan0102@utexas.edu>
2023-05-12 02:45:35 -0500

    Update SEO to follow personal website format

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,25 +1,32 @@
 import '../styles/globals.css'
 import { DefaultSeo } from 'next-seo'
 
+const defaultSEO = {
+  title: 'countdowns',
+  description:
+    'Make beautiful countdowns and share them with your friends!',
+  openGraph: {
+    type: 'website',
+    url: 'https://www.countdowns.live',
+    site_name: 'countdowns',
+    images: [
+      {
+        url: '/static/thumbnail.png',
+        alt: 'countdowns.live homepage',
+      },
+    ],
+  },
+  twitter: {
+    handle: '@ishan0102',
+    site: '@ishan0102',
+    cardType: 'summary_large_image',
+  },
+};
+
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <DefaultSeo
-        openGraph={{
-          type: 'website',
-          url: 'https://www.countdowns.live',
-          title: 'countdowns',
-          description: 'Make countdowns and share them with your friends.',
-          images: [
-            {
-              url: '/static/thumbnail.png',
-              width: 3456,
-              height: 1922,
-              alt: 'countdowns.live homepage',
-            },
-          ],
-        }}
-      />
+      <DefaultSeo {...defaultSEO} />
       <Component {...pageProps} />
     </>
   );
